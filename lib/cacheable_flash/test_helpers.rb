81dad2a00b5a915f9922f46da100a2080e680f6e
Garry Dolley <gdolley@arpnetworks.com>
2008-07-14 00:44:52 -0700

    I like symbols, not strings, when using flash_cookie in my tests; let's allow both

--- lib/cacheable_flash/test_helpers.rb
+++ lib/cacheable_flash/test_helpers.rb
@@ -2,7 +2,7 @@ module CacheableFlash
   module TestHelpers
     def flash_cookie
       return {} unless cookies['flash']
-      JSON.parse(cookies['flash'].first)
-    end    
+      HashWithIndifferentAccess.new(JSON.parse(cookies['flash'].first))
+    end
   end
 end
