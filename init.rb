48c730a651159ae5f44d882bc85ace6b134b25e5
pboling <peter.boling@peterboling.com>
2010-02-08 11:16:16 -0500

    Improved init.rb

--- init.rb
+++ init.rb
@@ -1,4 +1,13 @@
-gem "json", ">=1.1.2"
+if config.respond_to?(:gems)
+  config.gem 'json', :version => ">=1.1.2"
+else
+  begin
+    gem "json", ">=1.1.2"
+  rescue Gem::LoadError
+    puts "Install the json gem, version >= 1.1.2"
+  end
+end
+
 require "json"
 require "cacheable_flash"
 ActionView::Helpers::AssetTagHelper.register_javascript_include_default('cookie','flash','json')
