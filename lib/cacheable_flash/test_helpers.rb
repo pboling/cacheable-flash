bc1f52165df0c16339ecc3466d91df5ab8ce0c45
Peter Boling <peter.boling@gmail.com>
2012-08-10 20:38:53 -0300

    Update lib/cacheable_flash/test_helpers.rb

--- lib/cacheable_flash/test_helpers.rb
+++ lib/cacheable_flash/test_helpers.rb
@@ -8,5 +8,23 @@ module CacheableFlash
     rescue JSON::ParserError
       {}
     end
+    
+    module Rspec
+      RSpec::Matchers.define :have_flash_cookie do |flash_status, regex|
+        define_method :has_flash_cookie? do |response|
+          regex = /^#{Regexp.escape(regex)}$/ if regex.is_a?(String)
+  
+          cook = begin
+            response.cookies['flash'] ?
+              JSON(response.cookies['flash']) :
+              {}
+          rescue JSON::ParserError
+            {}
+          end
+          cook[flash_status] =~ regex
+        end
+        match{|response| has_flash_cookie?(response)}
+      end
+    end
   end
 end
