665d6518b25499a4b83b66536309cc34f14cb5b6
Peter Boling <peter.boling@gmail.com>
2012-08-10 20:34:32 -0300

    Cookie Flash is sticky until cleared by javascript

--- lib/cacheable_flash/middleware.rb
+++ lib/cacheable_flash/middleware.rb
@@ -8,19 +8,30 @@ module CacheableFlash
       @app = app
     end
 
+    # Cookies stick in the flash until rendered (cleared out of the cookie by javascript),
+    #   to ensure they are seen and not lost, so we grab them from the rails flash hash, or the request cookies
     def call(env)
       status, headers, body = @app.call(env)
       flash = env[FLASH_HASH_KEY]
 
+      request = ActionDispatch::Request.new(env)
+      cookie_flash = request.respond_to?(:cookie_jar) ?
+        request.cookie_jar['flash'] :
+        nil
+
       if flash
         response = Rack::Response.new(body, status, headers)
-
         cookies = env["rack.cookies"] || {}
         response.set_cookie("flash", { :value => cookie_flash(flash, cookies), :path => "/" })
+        response.finish
+      elsif cookie_flash
+        response = Rack::Response.new(body, status, headers)
+        response.set_cookie("flash", { :value => cookie_flash, :path => "/" })
         response.finish
       else
         [status, headers, body]
       end
     end
+
   end
 end
