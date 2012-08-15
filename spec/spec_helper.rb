fb37b6dee00674aaea554ff1fb646b95a54f6fc6
Peter Boling <peter.boling@gmail.com>
2012-08-15 10:51:23 -0400

    Update spec helper with rspec defaults

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,8 @@ require File.expand_path("../dummy/config/environment",  __FILE__)
 
 require 'rspec/rails'
 require "json"
+require 'cacheable_flash'
+require 'cacheable_flash/rspec_matchers'
 
 ENGINE_RAILS_ROOT=File.join(File.dirname(__FILE__), '../')
 
@@ -11,17 +13,16 @@ ENGINE_RAILS_ROOT=File.join(File.dirname(__FILE__), '../')
 # in spec/support/ and its subdirectories.
 Dir[File.join(ENGINE_RAILS_ROOT, "spec/support/**/*.rb")].each {|f| require f }
 
-require 'cacheable_flash'
-# Instead of loading the installed gem, we load the source code directly, would this work?
-#$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
-#$LOAD_PATH.unshift(File.dirname(__FILE__))
-
-require 'cacheable_flash/rspec_matchers'
-
-# Requires supporting files with custom matchers and macros, etc,
-# in ./support/ and its subdirectories.
-#Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
-
 RSpec.configure do |config|
+  config.treat_symbols_as_metadata_keys_with_true_values = true
+  config.run_all_when_everything_filtered = true
+  config.filter_run :focus
+
+  # Run specs in random order to surface order dependencies. If you find an
+  # order dependency and want to debug it, you can fix the order by providing
+  # the seed, which is printed after each run.
+  #     --seed 1234
+  config.order = 'random'
+
   config.include CacheableFlash::RspecMatchers
 end
