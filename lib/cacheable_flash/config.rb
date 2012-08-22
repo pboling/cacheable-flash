205d6089492e8c2f96dce2914b91d01531188e9b
Peter Boling <peter.boling@gmail.com>
2012-08-22 12:00:47 -0400

    Config now works!

--- lib/cacheable_flash/config.rb
+++ lib/cacheable_flash/config.rb
@@ -10,35 +10,34 @@ module CacheableFlash
     end
 
     DEFAULTS = {
+      :stacking => false, # or true if you want auto-magically stacking flashes
       # Specify how multiple flashes at the same key (e.g. :notice, :errors) should be handled
-      :append_as => :br, # or :array, :p, :ul, :ol, or a proc or lambda of your own design
+      # :append_as is ignored if :stacking is false
+      :append_as => :br # or :array, :p, :ul, :ol, or a proc or lambda of your own design
     }
 
-    self.config ||= DEFAULTS
+    @config ||= DEFAULTS
     def self.configure &block
-      yield @@config
-      StackableFlash::Config.configure do
-        if self.config[:append_as].respond_to?(:call)
-          config[:stack_with_proc] = self.config[:append_as]
-        else
-          config[:stack_with_proc] = case self.config[:append_as]
-            when :br then Proc.new {|arr| arr.join('<br/>') }
-            when :array then Proc.new {|arr| arr }
-            when :p then Proc.new {|arr| arr.map! {|x| "<p>#{x}</p>"}.join }
-            when :ul then Proc.new {|arr| '<ul>' + arr.map! {|x| "<li>#{x}</li>"}.join + '</ul>' }
-            when :ol then Proc.new {|arr| '<ol>' + arr.map! {|x| "<li>#{x}</li>"}.join + '</ol>' }
+      yield @config
+      if @config[:stacking]
+        StackableFlash.stacking = true
+        StackableFlash::Config.configure do
+            # by default behave like regular non-stacking flash
+          if @config[:append_as].respond_to?(:call)
+            @config[:stack_with_proc] = @config[:append_as]
+          else
+            @config[:stack_with_proc] = case @config[:append_as]
+              when :br then Proc.new {|arr| arr.join('<br/>') }
+              when :array then Proc.new {|arr| arr }
+              when :p then Proc.new {|arr| arr.map! {|x| "<p>#{x}</p>"}.join }
+              when :ul then Proc.new {|arr| '<ul>' + arr.map! {|x| "<li>#{x}</li>"}.join + '</ul>' }
+              when :ol then Proc.new {|arr| '<ol>' + arr.map! {|x| "<li>#{x}</li>"}.join + '</ol>' }
+            end
           end
         end
+      else
+        StackableFlash.stacking = false
       end
-    end
-
-    def self.[](key)
-      return nil unless key.respond_to?(:to_sym)
-      CacheableFlash::Config.config[key.to_sym]
-    end
-
-    def self.method_missing(name, *args)
-      CacheableFlash[name]
     end
 
   end
