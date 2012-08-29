22c85187ee2dfc2ac107d5f5fe20e841b38bce6d
Peter Boling <peter.boling@gmail.com>
2012-08-29 18:01:40 -0300

    Allow Rails 4

--- lib/cacheable_flash/engine.rb
+++ lib/cacheable_flash/engine.rb
@@ -1,6 +1,6 @@
 module CacheableFlash
   class Engine < ::Rails::Engine
-    if ::Rails::VERSION::MAJOR == 3 && ::Rails::VERSION::MAJOR >= 1
+    if ::Rails::VERSION::MAJOR > 3 || ::Rails::VERSION::MAJOR == 3 && ::Rails::VERSION::MAJOR >= 1
       isolate_namespace CacheableFlash
     end
   end
