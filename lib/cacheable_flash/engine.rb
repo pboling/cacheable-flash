dd44643d4d1b10176d436a72ba7761fdc30070ea
Peter Boling <peter.boling@gmail.com>
2012-08-29 18:04:29 -0300

    Add warning when fails to isolate namespace

--- lib/cacheable_flash/engine.rb
+++ lib/cacheable_flash/engine.rb
@@ -2,6 +2,8 @@ module CacheableFlash
   class Engine < ::Rails::Engine
     if ::Rails::VERSION::MAJOR > 3 || ::Rails::VERSION::MAJOR == 3 && ::Rails::VERSION::MAJOR >= 1
       isolate_namespace CacheableFlash
+    else
+      warn 'CacheableFlash namespace not isolated, check that Rails version is >= 3.1'
     end
   end
 end
