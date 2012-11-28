77b72dcf4f55ec14805901558784a703bef04046
dsgh <duarte.henriques@gmail.com>
2012-11-28 22:01:16 +0000

    fix hidden bug: don't call 'flash' from middleware

--- lib/cacheable_flash/middleware.rb
+++ lib/cacheable_flash/middleware.rb
@@ -17,7 +17,7 @@ module CacheableFlash
       if env_flash
         # There is a flash set from this request, merge it into the cookie flash (which may already be populated)
         cookies = Rack::Request.new(env).cookies
-        Rack::Utils.set_cookie_header!(headers, "flash", :value => cookie_flash(flash, cookies), :path => "/")
+        Rack::Utils.set_cookie_header!(headers, "flash", :value => cookie_flash(env_flash, cookies), :path => "/")
       else
         # There is no flash set in this request but there are leftovers from previous requests still in the cookie
         request = ActionDispatch::Request.new(env)
