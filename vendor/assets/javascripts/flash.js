3f10e75652ec231a5448c998ce9081488b9b6112
Pat McGee <patmcgee331@gmail.com>
2013-06-28 11:49:19 -0400

    test specific domain on flash reset

--- vendor/assets/javascripts/flash.js
+++ vendor/assets/javascripts/flash.js
@@ -7,7 +7,7 @@ Flash.transferFromCookies = function() {
   var data = JSON.parse(unescape($.cookie("flash")));
   if(!data) data = {};
   Flash.data = data;
-  $.cookie('flash',null, {path: '/'});
+  $.cookie('flash',null, {path: '/', domain: document.domain});
 };
 
 Flash.writeDataTo = function(name, element, callback) {
