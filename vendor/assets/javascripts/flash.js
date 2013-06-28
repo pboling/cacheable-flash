6819561a60c51dc4f92a6184d22f37e9fa689dc4
Pat McGee <patmcgee331@gmail.com>
2013-06-28 12:19:11 -0400

    domain reset

--- vendor/assets/javascripts/flash.js
+++ vendor/assets/javascripts/flash.js
@@ -7,7 +7,7 @@ Flash.transferFromCookies = function() {
   var data = JSON.parse(unescape($.cookie("flash")));
   if(!data) data = {};
   Flash.data = data;
-  $.cookie("flash", {path: '/', domain: document.domain});
+  $.cookie("flash",null, {path: '/', domain: document.domain });
 };
 
 Flash.writeDataTo = function(name, element, callback) {
