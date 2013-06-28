cd5ee41b2104bfbb849141a57e3ee0bcc0c0f088
Pat McGee <patmcgee331@gmail.com>
2013-06-28 15:24:47 -0400

    bleh

--- lib/cacheable_flash/middleware.rb
+++ lib/cacheable_flash/middleware.rb
@@ -16,11 +16,10 @@ module CacheableFlash
       if env_flash
         domain = CacheableFlash::Config.config[:domain]
         cookies = Rack::Request.new(env).cookies
-        Rack::Utils.set_cookie_header!(headers, "flash", :value => cookie_flash(env_flash, cookies), :path => "/", :domain => '#{domain}')
+        Rack::Utils.set_cookie_header!(headers, "flash", :value => cookie_flash(env_flash, cookies), :path => "/", :domain => "#{domain}")
       end
 
       [status, headers, body]
     end
-
   end
 end
