e6b9eda5ad0bb203dc754bb6f5a6ea0d9966735c
Jake Mitchell <jake3p0@gmail.com>
2016-06-29 15:29:12 -0400

    Moved away from jQuery cookie towards js-cookie as jQuery cookie is no longer supported and will eventually lead to developers using two cookie solutions in their code bases.

--- lib/cacheable_flash/railtie.rb
+++ lib/cacheable_flash/railtie.rb
@@ -3,7 +3,7 @@ module CacheableFlash
     # Add cacheable flash JS to defaults for Rails < 3.1 (not needed with asset pipeline)
     if ::Rails::VERSION::MAJOR == 3 && ::Rails::VERSION::MINOR == 0
       config.before_configuration do
-        config.action_view.javascript_expansions[:cacheable_flash] = %w(jquery.cookie flash)
+        config.action_view.javascript_expansions[:cacheable_flash] = %w(js.cookie flash)
       end
     end
   end
